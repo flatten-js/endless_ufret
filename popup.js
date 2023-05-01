9c4589765e243f4565c7474865d3a5cac216a01d
flatten-js <thetakahito@gmail.com>
2023-05-01 13:02:38 +0900

    Functional improvements have been made around the list display

--- popup.js
+++ popup.js
@@ -18,31 +18,41 @@
       chrome.tabs.sendMessage(tab.id, { tab_id: id })
     }
 
-    async function update_name_tab(id, name) {
-      chrome.storage.local.get(song_id, data => {
-        const tabs = JSON.parse(data[song_id] || '[]').map(tab => tab.id == id ? { ...tab, name } : tab)
-        chrome.storage.local.set({ [song_id]: JSON.stringify(tabs) })
+    async function get_tabs() {
+      return new Promise(resolve => {
+        chrome.storage.local.get(song_id, data => {
+          const tabs = JSON.parse(data[song_id] || '[]')
+          return resolve(tabs)
+        })
       })
+    }
+
+    async function update_name_tab(id, name) {
+      let tabs = await get_tabs()
+      tabs = tabs.map(tab => tab.id == id ? { ...tab, name } : tab)
+      chrome.storage.local.set({ [song_id]: JSON.stringify(tabs) })
+    }
+
+    async function delete_tab(id) {
+      let tabs = await get_tabs()
+      tabs = tabs.filter(tab => tab.id != id)
+      chrome.storage.local.set({ [song_id]: JSON.stringify(tabs) })
     }
   
     function on_change_handler(changes) {
       if (changes[song_id]) {
         const tabs = JSON.parse(changes[song_id].newValue || '[]')
   
-        points.innerHTML = tabs.map((tab, i) => `<li id="${tab.id}"><span>${i+1}.</span><input value="${tab.name}" /><i class="las la-step-backward"></i></li>`).join('')
+        points.innerHTML = tabs.map((tab, i) => `<li id="${tab.id}"><span>${i+1}.</span><input value="${tab.name}" /><i class="las la-step-backward"></i><i class="las la-times"></i></li>`).join('')
         points.querySelectorAll('li').forEach(li => {
           const input = li.querySelector('input')
-          
-          let timer
-          input.oninput = e => {
-            clearTimeout(timer)
-            timer = setTimeout(() => {
-              update_name_tab(li.id, e.target.value)
-            }, 1000)
-          }
+          input.onblur = e => update_name_tab(li.id, e.target.value)
 
-          const i = li.querySelector('i')
-          i.onclick = () => next_active_tab(li.id)
+          const back = li.querySelector('.la-step-backward')
+          back.onclick = () => next_active_tab(li.id)
+
+          const times = li.querySelector('.la-times')
+          times.ondblclick = () => delete_tab(li.id)
         })
       }
     }
